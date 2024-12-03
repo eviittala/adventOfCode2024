1a035d8f779c84d916dcf1f2235d4564fd7fa4e2
Eero Viittala <eero.viittala@outlook.com>
2024-12-03 22:39:00 +0200

    Day3 part2

--- day3/part2/Main.cpp
+++ day3/part2/Main.cpp
@@ -4,61 +4,35 @@
 
 #include "common.hpp"
 
-std::vector<std::vector<int32_t>> vectors;
+uint32_t getResult(const std::string& str) {
+    std::string tmp = str;
+    std::string tmp2;
+    std::regex re(R"(mul\((\d{1,3}),(\d{1,3})\))");
+    std::regex skip(R"((.*?)(don't\(\).*?do\(\)))");
+    std::smatch sm;
+    uint32_t res{};
 
-void parseData(const std::string& txt) {
-    std::istringstream in(txt);
-    std::regex re(R"((\d+))");
-    std::smatch match;
-    for (std::string line; std::getline(in, line);) {
-        std::vector<int32_t> tmp;
-        while (std::regex_search(line, match, re)) {
-            tmp.push_back(std::stoi(match[1].str().c_str()));
-            line = match.suffix();
-        }
-        vectors.push_back(std::move(tmp));
+    while (std::regex_search(tmp, sm, skip)) {
+        tmp2 += sm[1].str();
+        tmp = sm.suffix();
     }
-}
 
-bool checkData(const std::vector<int32_t>& vec) {
-    return (std::is_sorted(std::begin(vec), std::end(vec),
-                           [](const auto next, const auto first) {
-                               if (std::abs(next - first) > 3) return true;
-                               if (first == next) return true;
-                               return first > next;
-                           }) ||
-            std::is_sorted(std::begin(vec), std::end(vec),
-                           [](const auto next, const auto first) {
-                               if (std::abs(next - first) > 3) return true;
-                               if (first == next) return true;
-                               return next > first;
-                           })
-
-    );
-}
-
-uint32_t getResult() {
-    uint32_t result{};
-    for (const auto& vec : vectors) {
-        if (checkData(vec)) {
-            ++result;
-        } else {
-            for (size_t i{}; i < vec.size(); ++i) {
-                std::vector<int32_t> tmp(std::begin(vec), std::end(vec));
-                tmp.erase(std::begin(tmp) + i);
-                if (checkData(tmp)) {
-                    ++result;
-                    break;
-                }
-            }
-        }
+    if (std::regex_search(tmp, sm, std::regex(R"((.*?)don't\(\))"))) {
+        tmp = sm[1].str();
     }
-    return result;
+
+    tmp2 += tmp;
+
+    while (std::regex_search(tmp2, sm, re)) {
+        res += std::stoi(sm[1].str()) * std::stoi(sm[2].str());
+        tmp2 = sm.suffix();
+    }
+
+    return res;
 }
 
 int main(int args, char* argv[]) {
     auto str = common::readFile(common::getInputFileDir(__FILE__));
-    parseData(str);
-    std::cout << "Result: " << getResult() << std::endl;
+    std::cout << "Result: " << getResult(str) << std::endl;
     return 0;
 }
