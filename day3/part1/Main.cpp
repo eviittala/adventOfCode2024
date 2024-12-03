2a49accd5e1ebfbeab6107ec87dac77801549611
Eero Viittala <eero.viittala@outlook.com>
2024-12-03 17:25:43 +0200

    Day3 Part1

--- day3/part1/Main.cpp
+++ day3/part1/Main.cpp
@@ -4,48 +4,20 @@
 
 #include "common.hpp"
 
-std::vector<std::vector<int32_t>> vectors;
-
-void parseData(const std::string& txt) {
-    std::istringstream in(txt);
-    std::regex re(R"((\d+))");
-    std::smatch match;
-    for (std::string line; std::getline(in, line);) {
-        std::vector<int32_t> tmp;
-        while (std::regex_search(line, match, re)) {
-            tmp.push_back(std::stoi(match[1].str().c_str()));
-            line = match.suffix();
-        }
-        vectors.push_back(std::move(tmp));
+uint32_t getResult(const std::string& str) {
+    std::string tmp = str;
+    std::regex re(R"(mul\((\d{1,3}),(\d{1,3})\))");
+    std::smatch sm;
+    uint32_t res{};
+    while (std::regex_search(tmp, sm, re)) {
+        res += std::stoi(sm[1].str()) * std::stoi(sm[2].str());
+        tmp = sm.suffix();
     }
-}
-
-uint32_t getResult() {
-    uint32_t result{};
-    for (const auto& vec : vectors) {
-        if (std::is_sorted(std::begin(vec), std::end(vec),
-                           [](const auto next, const auto first) {
-                               if (std::abs(next - first) > 3) return true;
-                               if (first == next) return true;
-                               return first > next;
-                           }) ||
-            std::is_sorted(std::begin(vec), std::end(vec),
-                           [](const auto next, const auto first) {
-                               if (std::abs(next - first) > 3) return true;
-                               if (first == next) return true;
-                               return next > first;
-                           })
-
-        ) {
-            ++result;
-        }
-    }
-    return result;
+    return res;
 }
 
 int main(int args, char* argv[]) {
     auto str = common::readFile(common::getInputFileDir(__FILE__));
-    parseData(str);
-    std::cout << "Result: " << getResult() << std::endl;
+    std::cout << "Result: " << getResult(str) << std::endl;
     return 0;
 }
