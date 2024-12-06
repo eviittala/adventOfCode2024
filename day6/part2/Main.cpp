0e079cbdbf713f8fd14a103fbf022e82e8c6e959
Eero Viittala <eero.viittala@outlook.com>
2024-12-06 16:34:51 +0200

    Day6 part2

--- day6/part2/Main.cpp
+++ day6/part2/Main.cpp
@@ -1,11 +1,53 @@
 #include <cstdint>
 #include <iostream>
 #include <regex>
+#include <set>
 
 #include "common.hpp"
 
-uint32_t getResult(const std::string& str) {
-    uint32_t res{};
+std::vector<int> makeDirs(const int len) { return {-len, 1, len, -1}; }
+
+uint32_t getResult(std::string& str) {
+    const int len = str.find('\n') + 1;
+    const auto dirs = makeDirs(len);
+    std::set<std::pair<int, int>> visit;
+    int dir{0};
+    int res{};
+    const int startPos = str.find('^');
+
+    for (int i{}; i < str.size(); ++i) {
+        if (str.at(i) == '\n' || i == startPos || str.at(i) != '.') {
+            continue;
+        }
+
+        str.at(i) = '0';
+
+        int pos = startPos;
+        dir = 0;
+        visit.clear();
+
+        while (true) {
+            if (!visit.insert({pos, dir}).second) {
+                ++res;
+                break;
+            }
+            const int newPos = pos + dirs.at(dir);
+
+            if (newPos < 0 || newPos >= str.length() ||
+                str.at(newPos) == '\n') {
+                break;
+            }
+
+            if (str.at(newPos) == '.' || str.at(newPos) == '^') {
+                pos = newPos;
+            } else {
+                dir = (dir + 1) % dirs.size();
+            }
+        }
+
+        str.at(i) = '.';
+    }
+
     return res;
 }
 
