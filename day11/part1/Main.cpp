541eee9cbe58b6d89cafdf0b81fffa5a8a50b15c
Eero Viittala <eero.viittala@outlook.com>
2024-12-10 20:15:34 +0200

    template for day11

---
+++ day11/part1/Main.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <numeric>
+#include <regex>
+#include <set>
+#include <unordered_map>
+
+#include "common.hpp"
+
+uint64_t getResult(const std::string& str) {
+    uint64_t scores{};
+
+    return scores;
+}
+
+int main(int args, char* argv[]) {
+    const auto str = common::readFile(common::getInputFileDir(__FILE__));
+    std::cout << "Result: " << getResult(str) << std::endl;
+    return 0;
+}
