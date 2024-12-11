c33d5f16fe5600183e345ce68cee8b3e0de01d79
Eero Viittala <eero.viittala@outlook.com>
2024-12-11 19:15:45 +0200

    Day11 part1

--- day11/part1/Main.cpp
+++ day11/part1/Main.cpp
@@ -8,10 +8,43 @@
 
 #include "common.hpp"
 
-uint64_t getResult(const std::string& str) {
-    uint64_t scores{};
+bool evenNbrOfDigits(const uint64_t val) {
+    return (std::to_string(val).size() % 2) == 0;
+}
 
-    return scores;
+uint64_t getLeftHalf(const uint64_t val) {
+    const std::string str{std::to_string(val)};
+    const size_t size = str.size();
+    return std::stoll(str.substr(0, size / 2));
+}
+
+uint64_t getRightHalf(const uint64_t val) {
+    const std::string str{std::to_string(val)};
+    const size_t size = str.size();
+    return std::stoll(str.substr(size / 2));
+}
+
+uint64_t getResult(const std::string& str) {
+    auto data = common::parseUintData(str).at(0);
+
+    std::vector<uint64_t> temp;
+
+    for (size_t blinks{}; blinks < 25; ++blinks) {
+        for (size_t i{}; i < data.size(); ++i) {
+            const uint64_t val{data.at(i)};
+            if (val == 0) {
+                temp.push_back(val + 1);
+            } else if (evenNbrOfDigits(val)) {
+                temp.push_back(getLeftHalf(val));
+                temp.push_back(getRightHalf(val));
+            } else {
+                temp.push_back(val * 2024);
+            }
+        }
+        data = std::move(temp);
+    }
+
+    return data.size();
 }
 
 int main(int args, char* argv[]) {
