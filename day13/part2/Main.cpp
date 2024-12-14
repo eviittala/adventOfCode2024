756f8cb45e6c0da9a4dbae0a26294636c9d5be31
Eero Viittala <eero.viittala@outlook.com>
2024-12-14 22:59:51 +0200

    Day13 part2

--- day13/part2/Main.cpp
+++ day13/part2/Main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cmath>
 #include <cstdint>
 #include <iostream>
 #include <numeric>
@@ -8,138 +9,80 @@
 
 #include "common.hpp"
 
-size_t lineLength{};
+struct Token {
+    uint64_t a_x{};
+    uint64_t a_y{};
+    uint64_t b_y{};
+    uint64_t b_x{};
+    uint64_t p_x{};
+    uint64_t p_y{};
+};
 
-bool isValidPos(const std::string& str, const int pos) {
-    return 0 <= pos && pos < (int)str.size() && str[pos] != '\n';
-}
+void printData(const std::vector<Token>& vec) {
+    std::cout << "Printing the vec" << std::endl;
 
-std::set<int> visited;
-std::set<int> getPositions(const std::string& str, const int pos,
-                           const char c) {
-    if (!isValidPos(str, pos) || str[pos] != c) {
-        return {};
+    for (const auto& token : vec) {
+        std::cout << "A_x: " << token.a_x << ", ";
+        std::cout << "A_y: " << token.a_y << ", ";
+        std::cout << "B_x: " << token.b_x << ", ";
+        std::cout << "B_y: " << token.b_y << ", ";
+        std::cout << "P_x: " << token.p_x << ", ";
+        std::cout << "P_y: " << token.p_y;
+        std::cout << std::endl;
     }
-    if (!visited.insert(pos).second) {
-        return {};
-    }
-    std::set<int> ret{pos};
 
-    for (auto dir : common::getDirections2d(lineLength)) {
-        auto positions = getPositions(str, pos + dir, c);
-        ret.insert(std::begin(positions), std::end(positions));
-    }
-    return ret;
-}
-
-uint64_t getArea(const std::set<int> positions) { return positions.size(); }
-
-void printSet(const std::set<int>& set) {
-    for (const auto i : set) {
-        std::cout << i << ", ";
-    }
     std::cout << std::endl;
 }
 
-int getMin(const std::set<int>& positions) {
-    int ret{INT32_MAX};
-    for (const auto pos : positions) {
-        if (pos < ret) ret = pos;
+std::vector<Token> parseData(const std::string& str) {
+    std::vector<Token> ret;
+    std::regex re(
+        R"(Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)\n)");
+    std::string temp = str;
+    std::smatch sm;
+
+    while (std::regex_search(temp, sm, re)) {
+        Token token;
+        token.a_x = std::stoull(sm[1].str());
+        token.a_y = std::stoull(sm[2].str());
+        token.b_x = std::stoull(sm[3].str());
+        token.b_y = std::stoull(sm[4].str());
+        token.p_x = std::stoull(sm[5].str()) + 10000000000000;
+        token.p_y = std::stoull(sm[6].str()) + 10000000000000;
+        ret.push_back(token);
+        temp = sm.suffix();
     }
     return ret;
-}
-
-int getMax(const std::set<int>& positions) {
-    int ret{};
-    for (const auto pos : positions) {
-        if (pos > ret) ret = pos;
-    }
-    return ret;
-}
-
-uint64_t getPerimeter(const std::string& str, const std::set<int> positions) {
-    uint64_t ret{};
-    std::vector<std::pair<uint64_t, std::bitset<4>>> pos_t;
-
-    for (auto pos : positions) {
-        std::bitset<4> sides{0b1111};
-
-        for (auto dir : common::getDirections2d(lineLength)) {
-            const int newPos = pos + dir;
-            if (!isValidPos(str, newPos)) continue;
-            if (positions.contains(newPos)) {
-                if (dir == -(int)lineLength) {
-                    sides &= 0b0111;
-                } else if (dir == 1) {
-                    sides &= 0b1011;
-                } else if (dir == (int)lineLength) {
-                    sides &= 0b1101;
-                } else {
-                    sides &= 0b1110;
-                }
-            }
-        }
-        pos_t.emplace_back(pos, sides);
-
-        ret += sides.count();
-    }
-    std::sort(std::begin(pos_t), std::end(pos_t),
-              [](const auto a, const auto b) { return a.first < b.first; });
-    for (size_t i{1}; i < pos_t.size(); ++i) {
-        const auto pre = pos_t.at(i - 1);
-        const auto cur = pos_t.at(i);
-        if (pre.first + 1 == cur.first && pre.second.test(3) &&
-            cur.second.test(3)) {
-            ret -= 1;
-        }
-        if (pre.first + 1 == cur.first && pre.second.test(1) &&
-            cur.second.test(1)) {
-            ret -= 1;
-        }
-    }
-
-    for (const auto& posPair : pos_t) {
-        const auto& pre = posPair;
-        if (pre.second.test(0)) {
-            const auto it = std::find_if(
-                std::begin(pos_t), std::end(pos_t), [&pre](const auto& a) {
-                    return pre.first - lineLength == a.first &&
-                           a.second.test(0);
-                });
-            if (it != std::end(pos_t)) ret -= 1;
-        }
-        if (pre.second.test(2)) {
-            const auto it = std::find_if(
-                std::begin(pos_t), std::end(pos_t), [&pre](const auto& a) {
-                    return pre.first - lineLength == a.first &&
-                           a.second.test(2);
-                });
-            if (it != std::end(pos_t)) ret -= 1;
-        }
-    }
-    return ret;
-}
-
-uint64_t getPrice(const std::string& str, const int pos) {
-    if (visited.contains(pos) || !isValidPos(str, pos)) {
-        return 0;
-    }
-    auto positions = getPositions(str, pos, str.at(pos));
-    visited.insert(std::begin(positions), std::end(positions));
-    if (positions.size() == 1) return 4;
-    return getArea(positions) * getPerimeter(str, positions);
 }
 
 uint64_t getResult(const std::string& str) {
-    uint64_t totalPrice{};
-    lineLength = str.find('\n') + 1;
+    uint64_t res{};
+    const auto machines = parseData(str);
+    // printData(machines);
 
-    for (size_t i{}; i < str.size(); ++i) {
-        if (!isValidPos(str, i)) continue;
-        const auto price = getPrice(str, i);
-        totalPrice += price;
+    for (const auto& machine : machines) {
+        uint64_t price{};
+
+        const double a_x = machine.a_x;
+        const double a_y = machine.a_y;
+        const double b_x = machine.b_x;
+        const double b_y = machine.b_y;
+        const double p_x = machine.p_x;
+        const double p_y = machine.p_y;
+
+        const double press_a =
+            (p_x * b_y - p_y * b_x) / (a_x * b_y - a_y * b_x);
+        const double press_b = (p_x - a_x * press_a) / b_x;
+
+        if (std::round(press_a) == press_a && std::round(press_b) == press_b) {
+            const uint64_t price_t = press_a * 3 + press_b * 1;
+            price = price_t;
+        }
+
+        res += price;
     }
-    return totalPrice;
+
+    return res;
 }
 
 int main(int args, char* argv[]) {
