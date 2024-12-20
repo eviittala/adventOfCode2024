9cdc048670351da9552d8988ad7e8c7899a9862c
Eero Viittala <eero.viittala@outlook.com>
2024-12-20 21:00:12 +0200

    My own day16 part2

--- day16/part2/Main.cpp
+++ day16/part2/Main.cpp
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <iostream>
 #include <numeric>
+#include <queue>
 #include <regex>
 #include <set>
 #include <unordered_map>
@@ -13,13 +14,25 @@
 struct Point {
     bool visited{};
     uint64_t steps{UINT64_MAX};
-    uint64_t tiles{UINT64_MAX};
     std::pair<int, int> dir{};
-    std::vector<std::pair<int, int>> pre{};
+    std::vector<std::pair<int, int>> pre;
 };
 
 std::map<std::pair<int, int>, Point> points;
 size_t line_size{};
+
+void printPoints() {
+    for (const auto& [pos, point] : points) {
+        std::cout << "(" << pos.first << ", " << pos.second << "), ";
+        std::cout << point.steps << ", ";
+        std::cout << "(" << point.dir.first << ", " << point.dir.second
+                  << "), ";
+        for (const auto& pre : point.pre) {
+            std::cout << "(" << pre.first << ", " << pre.second << "), ";
+        }
+        std::cout << std::endl;
+    }
+}
 
 std::vector<std::string> parseData(const std::string& str) {
     std::regex re(R"((#.*#))");
@@ -35,9 +48,20 @@ std::vector<std::string> parseData(const std::string& str) {
     return ret;
 }
 
-std::pair<size_t, size_t> getStart(const std::vector<std::string>& vec) {
+std::pair<int, int> getStart(const std::vector<std::string>& vec) {
     for (size_t y{}; y < vec.size(); ++y) {
         const size_t pos = vec.at(y).find('S');
+        if (pos != std::string::npos) {
+            return {pos, y};
+        }
+    }
+    assert(0);
+    return {};
+}
+
+std::pair<size_t, size_t> getEnd(const std::vector<std::string>& vec) {
+    for (size_t y{}; y < vec.size(); ++y) {
+        const size_t pos = vec.at(y).find('E');
         if (pos != std::string::npos) {
             return {pos, y};
         }
@@ -51,7 +75,7 @@ std::pair<int, int> plus(std::pair<int, int> a, std::pair<int, int> b) {
 }
 
 std::pair<int, int> getSmallest() {
-    uint64_t steps{INT32_MAX};
+    uint64_t steps{UINT64_MAX};
     std::pair<int, int> ret;
 
     for (auto& [pos, p] : points) {
@@ -63,23 +87,33 @@ std::pair<int, int> getSmallest() {
     return ret;
 }
 
-std::set<std::pair<int, int>> getTiles(const std::vector<std::string>& vec,
-                                       const std::pair<int, int> cur) {
-    auto& p = points.at(cur);
-    std::set<std::pair<int, int>> ret{cur};
-    if (vec.at(cur.second)[cur.first] == 'S') return ret;
+std::set<std::pair<int, int>> tiles;
 
-    for (const auto& pre : p.pre) {
-        const auto pre_tiles = getTiles(vec, pre);
-        ret.insert(std::begin(pre_tiles), std::end(pre_tiles));
+void getTiles(const std::vector<std::string>& vec,
+              const std::pair<int, int> end, const bool firstRun) {
+    // auto pos = getEnd(vec);
+    // do {
+    //     tiles.insert(pos);
+    //     pos = points.at(pos).pre;
+    // } while (pos.first != INT32_MAX && pos.second != INT32_MAX);
+
+    if (!tiles.insert(end).second && !firstRun) {
+        // std::cout << "end: " << end.first << ", " << end.second << std::endl;
+        return;
     }
-    return ret;
+    if (vec.at(end.second)[end.first] == 'S') return;
+
+    for (const auto& pre : points.at(end).pre) {
+        getTiles(vec, pre, false);
+        // ret.insert(std::begin(pre_tiles), std::end(pre_tiles));
+    }
+    // return ret;
 }
 
 void fillMapAndPrint(const std::vector<std::string>& vec,
                      std::pair<int, int> cur) {
     auto temp = vec;
-    const auto tiles = getTiles(vec, cur);
+    // const auto tiles = getTiles(vec, cur);
 
     for (const auto& tile : tiles) {
         temp.at(tile.second)[tile.first] = 'O';
@@ -96,24 +130,114 @@ uint64_t getNextSteps(const std::pair<int, int> c_dir,
     return 1001;
 }
 
-std::vector<std::pair<int, int>> getDirs(const std::pair<int, int> cur) {
-    const std::pair<int, int> up{0, -1};
-    const std::pair<int, int> right{1, 0};
-    const std::pair<int, int> down{0, 1};
-    const std::pair<int, int> left{-1, 0};
+// std::vector<std::pair<int, int>> getDirs(const std::pair<int, int> cur) {
+std::vector<std::tuple<uint64_t, std::pair<int, int>>> getDirs(
+    const std::pair<int, int> cur) {
+    return {{1, {cur.first, cur.second}},
+            {1001, {cur.second, -cur.first}},
+            {1001, {-cur.second, cur.first}}};
+}
 
-    if (cur == up) {
-        return {left, up, right};
+void getRoute(const std::vector<std::string>& vec,
+              const std::pair<int, int> cur) {
+    auto& p = points.at(cur);
+
+    if (!p.visited) {
+        for (const auto& [steps, n_dir] : getDirs(p.dir)) {
+            auto next = plus(cur, n_dir);
+            if (vec.at(next.second)[next.first] == '#') continue;
+            auto& n_point = points[next];
+            const uint64_t n_steps = p.steps + steps;
+            if (n_steps <= n_point.steps) {
+                n_point.steps = n_steps;
+                n_point.pre.push_back(cur);
+                //        if (n_point.pre.size() > 1) {
+                //            std::cout << "Size: " << n_point.pre.size() << ":
+                //            "
+                //                      << next.first << ", " << next.second <<
+                //                      std::endl;
+                //        }
+                n_point.dir = n_dir;
+            } else if (n_point.dir != n_dir &&
+                       n_point.steps >= (n_steps - 1000)) {
+                n_point.pre.push_back(cur);
+            }
+        }
     }
-    if (cur == right) {
-        return {up, right, down};
+    p.visited = true;
+}
+
+bool areAllPointsVisited() {
+    for (const auto& point : points) {
+        if (!point.second.visited) {
+            return false;
+        }
     }
-    if (cur == down) {
-        return {left, down, right};
+    return true;
+}
+
+void findAlternativeRoutes(const std::vector<std::string>& vec) {
+    // const auto tiles_t = tiles;
+    const auto points_t = points;
+    const auto posStart = getStart(vec);
+    auto posEnd = getEnd(vec);
+    uint64_t c{};
+
+    auto pos = points_t.at(posEnd).pre.at(0);
+
+    while (pos != posStart) {
+        printf("%lu:%lu\n", c++, tiles.size());
+        points.clear();
+        points[posStart].steps = 0;
+        points[posStart].dir = std::pair(1, 0);
+        while (!areAllPointsVisited()) {
+            auto cur = getSmallest();
+            if (cur == pos) {
+                points[cur].visited = true;
+            } else {
+                getRoute(vec, cur);
+            }
+        }
+        getTiles(vec, pos, true);
+        // if (pos.first == 5 && pos.second == 7) {
+        //     printPoints();
+        // }
+        pos = points_t.at(pos).pre.at(0);
     }
-    if (cur == left) {
-        return {left, up, down};
-    }
+
+    /*
+        pos = points_t.at(pos).pre;
+        while (points_t.at(pos).pre.first != INT32_MAX &&
+               points_t.at(pos).pre.second != INT32_MAX) {
+            for (auto startDir : getDirsStart(points_t.at(pos).dir)) {
+                auto cur = pos;
+                points.clear();
+                points[cur].steps = 0;
+                points[cur].dir = startDir;
+                while (vec.at(cur.second)[cur.first] != 'E') {
+                    auto& p = points.at(cur);
+                    if (!p.visited) {
+                        p.visited = true;
+                        for (auto n_dir : getDirs(p.dir)) {
+                            auto next = plus(cur, n_dir);
+                            if (vec.at(next.second)[next.first] == '#')
+       continue; auto& n_point = points[next]; const uint64_t n_steps =
+       p.steps
+       + getNextSteps(p.dir, n_dir); if (n_steps <= n_point.steps) {
+                                n_point.steps = n_steps;
+                                n_point.pre = cur;
+                                n_point.dir = n_dir;
+                            }
+                        }
+                    }
+                    cur = getSmallest();
+                }
+            }
+
+            getTiles(vec, {});
+            pos = points_t.at(pos).pre;
+        }
+        */
 }
 
 uint64_t getResult(const std::string& str) {
@@ -125,36 +249,22 @@ uint64_t getResult(const std::string& str) {
     point.visited = false;
     // point.pre = std::pair(INT32_MAX, INT32_MAX);
     point.steps = 0;
-    point.tiles = 1;
+    // point.tiles = 1;
     point.dir = std::pair(1, 0);
 
     while (data.at(cur.second)[cur.first] != 'E') {
-        auto& p = points.at(cur);
-
-        if (!p.visited) {
-            p.visited = true;
-            const auto tiles = p.tiles;
-            for (auto n_dir : getDirs(p.dir)) {
-                auto next = plus(cur, n_dir);
-                if (data.at(next.second)[next.first] == '#') continue;
-                auto& n_point = points[next];
-                const uint64_t n_steps = p.steps + getNextSteps(p.dir, n_dir);
-                if (n_steps <= n_point.steps) {
-                    n_point.steps = n_steps;
-                    n_point.pre.push_back(cur);
-                    n_point.dir = n_dir;
-                    n_point.tiles = tiles + 1;
-                } else if (tiles <= n_point.tiles) {
-                    n_point.pre.push_back(cur);
-                }
-            }
-        }
+        // while (!areAllPointsVisited()) {
+        getRoute(data, cur);
         cur = getSmallest();
     }
     // std::cout << "Returning: " << data.at(cur.second)[cur.first] <<
     // std::endl;
-    // fillMapAndPrint(data, cur);
-    return getTiles(data, cur).size();
+    cur = getEnd(data);
+    std::cout << "steps: " << points.at(cur).steps << std::endl;
+    getTiles(data, cur, true);
+    // findAlternativeRoutes(data);
+    fillMapAndPrint(data, cur);
+    return tiles.size();
 }
 
 int main(int args, char* argv[]) {
@@ -162,4 +272,5 @@ int main(int args, char* argv[]) {
     std::cout << "Result: " << getResult(str) << std::endl;
     return 0;
 }
-// 130536
+// 584  // too low
+// 585  // too low
