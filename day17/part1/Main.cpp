16f8d171aeef4499e533c0e112db427c3b37df1b
Eero Viittala <eero.viittala@outlook.com>
2024-12-21 20:41:33 +0200

    Day17 part1

--- day17/part1/Main.cpp
+++ day17/part1/Main.cpp
@@ -10,126 +10,141 @@
 
 #include "Common.hpp"
 
-struct Point {
-    bool visited{};
-    uint64_t steps{UINT64_MAX};
-    std::pair<int, int> dir{};
-    std::pair<int, int> pre{};
+struct Registers {
+    uint64_t A{};
+    uint64_t B{};
+    uint64_t C{};
 };
 
-std::map<std::pair<int, int>, Point> points;
-size_t line_size{};
-
-std::vector<std::string> parseData(const std::string& str) {
-    std::regex re(R"((#.*#))");
-    std::vector<std::string> ret;
+Registers parseRegisters(const std::string& str) {
+    std::regex reA(R"(Register A:\s(\d+))");
+    std::regex reB(R"(Register B:\s(\d+))");
+    std::regex reC(R"(Register C:\s(\d+))");
     std::smatch sm;
-    std::string tmp = str;
+    Registers ret;
 
-    while (std::regex_search(tmp, sm, re)) {
-        ret.push_back(sm[1].str());
-        tmp = sm.suffix();
+    if (std::regex_search(str, sm, reA)) {
+        ret.A = std::stoll(sm[1].str());
+    }
+
+    if (std::regex_search(str, sm, reB)) {
+        ret.B = std::stoll(sm[1].str());
+    }
+
+    if (std::regex_search(str, sm, reC)) {
+        ret.C = std::stoll(sm[1].str());
     }
 
     return ret;
 }
 
-std::pair<size_t, size_t> getStart(const std::vector<std::string>& vec) {
-    for (size_t y{}; y < vec.size(); ++y) {
-        const size_t pos = vec.at(y).find('S');
-        if (pos != std::string::npos) {
-            return {pos, y};
+std::vector<uint64_t> parseProgram(const std::string& str) {
+    std::regex re(R"(Program:\s(.*))");
+    std::smatch sm;
+    std::vector<uint64_t> ret;
+
+    if (std::regex_search(str, sm, re)) {
+        // std::cout << sm[1].str() << std::endl;
+        std::string temp = sm[1].str();
+        while (std::regex_search(temp, sm, std::regex(R"((\d+))"))) {
+            ret.push_back(std::stoll(sm[1].str()));
+            temp = sm.suffix();
         }
     }
-    assert(0);
+
+    return ret;
+}
+
+std::vector<uint64_t> doCmd(const uint64_t opcode, const uint64_t combo,
+                            Registers& regs, size_t& pc) {
+    auto getOperand = [&regs, &combo]() -> uint64_t {
+        if (combo < 4) return combo;
+        switch (combo) {
+            case 4:
+                return regs.A;
+            case 5:
+                return regs.B;
+            case 6:
+                return regs.B;
+        }
+        return combo;
+    };
+    switch (opcode) {
+        case 0: {
+            const uint64_t num = regs.A;
+            regs.A = num / std::pow(2, getOperand());
+        } break;
+        case 1: {
+            regs.B ^= getOperand();
+        } break;
+        case 2: {
+            regs.B = getOperand() % 8;
+        } break;
+        case 3: {
+            if (regs.A != 0) {
+                pc = combo;
+                return {};
+            }
+        } break;
+        case 4: {
+            regs.B = regs.B ^ regs.C;
+        } break;
+        case 5: {
+            std::string str = std::to_string(getOperand() % 8);
+            std::vector<uint64_t> ret;
+            for (size_t i{}; i < str.size(); ++i) {
+                ret.push_back(str[i] - '0');
+            }
+            pc += 2;
+            return ret;
+        } break;
+        case 6: {
+            const uint64_t num = regs.A;
+            regs.B = num / std::pow(2, getOperand());
+        } break;
+        case 7: {
+            const uint64_t num = regs.A;
+            regs.C = num / std::pow(2, getOperand());
+        } break;
+    }
+    pc += 2;
     return {};
 }
 
-std::pair<int, int> plus(std::pair<int, int> a, std::pair<int, int> b) {
-    return {a.first + b.first, a.second + b.second};
-}
+void getResult(const std::string& str) {
+    auto regs = parseRegisters(str);
+    auto prog = parseProgram(str);
+    size_t pc{};
 
-std::pair<int, int> getSmallest() {
-    uint64_t steps{INT32_MAX};
-    std::pair<int, int> ret;
+    // printf("Register A:%lu\nRegister B:%lu\nRegister C:%lu\n", regs.A,
+    // regs.B,
+    //        regs.C);
+    // std::cout << "Program: ";
+    // for (auto i : prog) {
+    //     std::cout << i << ", ";
+    // }
+    // std::cout << std::endl;
+    std::vector<uint64_t> output;
 
-    for (auto& [pos, p] : points) {
-        if (!p.visited && p.steps < steps) {
-            steps = p.steps;
-            ret = pos;
+    while (pc < prog.size()) {
+        const uint64_t opcode = prog.at(pc);
+        const uint64_t combo = prog.at(pc + 1);
+        const auto res = doCmd(opcode, combo, regs, pc);
+        std::copy(std::begin(res), std::end(res), std::back_inserter(output));
+    }
+
+    for (size_t i{}; i < output.size(); ++i) {
+        std::cout << output.at(i);
+        if ((i + 1) < output.size()) {
+            std::cout << ",";
         }
     }
-    return ret;
-}
-
-void fillMapAndPrint(const std::vector<std::string>& vec,
-                     std::pair<int, int> cur) {
-    auto temp = vec;
-
-    while (true) {
-        auto& p = points.at(cur);
-        cur = p.pre;
-        std::cout << "cur: " << cur.first << ", " << cur.second << std::endl;
-        if (cur.first == INT32_MAX && cur.second == INT32_MAX) break;
-        if (temp.at(cur.second)[cur.first] == 'S') break;
-        temp.at(cur.second)[cur.first] = '+';
-    }
-
-    common::printVecStr(temp);
-}
-
-uint64_t getNextSteps(const std::pair<int, int> c_dir,
-                      const std::pair<int, int> n_dir) {
-    if (c_dir == n_dir) {
-        return 1;
-    }
-    return 1001;
-}
-
-std::vector<std::pair<int, int>> getDirs() {
-    return {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
-}
-
-uint64_t getResult(const std::string& str) {
-    auto data = parseData(str);
-    line_size = data.at(0).size();
-    // common::printVecStr(data);
-    auto cur = getStart(data);
-    auto& point = points[cur];
-    point.visited = false;
-    point.pre = std::pair(INT32_MAX, INT32_MAX);
-    point.steps = 0;
-    point.dir = std::pair(1, 0);
-
-    while (data.at(cur.second)[cur.first] != 'E') {
-        // std::cout << "cur: " << cur.first << ", " << cur.second << std::endl;
-        auto& p = points.at(cur);
-
-        if (!p.visited) {
-            p.visited = true;
-            for (auto n_dir : getDirs()) {
-                auto next = plus(cur, n_dir);
-                if (data.at(next.second)[next.first] == '#') continue;
-                const auto c_dir = p.dir;
-                auto& n_point = points[next];
-                const uint64_t n_steps = p.steps + getNextSteps(c_dir, n_dir);
-                if (n_steps < n_point.steps) {
-                    n_point.steps = n_steps;
-                    n_point.pre = cur;
-                    n_point.dir = n_dir;
-                }
-            }
-        }
-        cur = getSmallest();
-    }
-    // std::cout << "Returning: " << data.at(cur.second)[cur.first] <<
-    // std::endl; fillMapAndPrint(data, cur);
-    return points.at(cur).steps;
+    std::cout << std::endl;
 }
 
 int main(int args, char* argv[]) {
     const auto str = common::readFile(common::getInputFileDir(__FILE__));
-    std::cout << "Result: " << getResult(str) << std::endl;
+    std::cout << "Result: ";
+    getResult(str);
     return 0;
 }
-// 130536
