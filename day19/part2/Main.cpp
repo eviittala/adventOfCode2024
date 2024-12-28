69d411822366068560d14e3768bc2f089460914d
Eero Viittala <eero.viittala@outlook.com>
2024-12-28 20:37:54 +0200

    day17 part2 trying

--- day19/part2/Main.cpp
+++ day19/part2/Main.cpp
@@ -33,19 +33,82 @@ void parseData(const std::string& str) {
     }
 }
 
-bool isPossible(const std::string stripe) {
-    if (stripe.size() == 0) return true;
+std::vector<std::vector<std::string>> getWays(const std::string& stripe) {
+    size_t pos{};
+    pos = stripe.find("brwrr");
+    if (pos == 0) {
+        return {{{"b"}, {"r"}, {"wr"}, {"r"}}, {{"br", "wr", "r"}}};
+    }
+    pos = stripe.find("bggr");
+    if (pos == 0) {
+        return {{{"b"}, {"g"}, {"g"}, {"r"}}};
+    }
+    pos = stripe.find("gbbr");
+    if (pos == 0) {
+        return {
+            {{"g"}, {"b"}, {"b"}, {"r"}},
+            {{"g"}, {"b"}, {"br"}},
+            {{"gb"}, {"b"}, {"r"}},
+            {{"gb"}, {"br"}},
+        };
+    }
+    pos = stripe.find("rrbgbr");
+    if (pos == 0) {
+        return {
+            {{"r"}, {"r"}, {"b"}, {"g"}, {"b"}, {"r"}},
+            {{"r"}, {"r"}, {"b"}, {"g"}, {"br"}},
+            {{"r"}, {"r"}, {"b"}, {"gb"}, {"r"}},
+            {{"r"}, {"rb"}, {"g"}, {"b"}, {"r"}},
+            {{"r"}, {"rb"}, {"g"}, {"br"}},
+            {{"r"}, {"rb"}, {"gb"}, {"r"}},
+        };
+    }
+    pos = stripe.find("bwurrg");
+    if (pos == 0) {
+        return {{{"bwu"}, {"r"}, {"r"}, {"g"}}};
+    }
+    pos = stripe.find("brgr");
+    if (pos == 0) {
+        return {{{"b"}, {"r"}, {"g"}, {"r"}}, {{"br", "g", "r"}}};
+    }
+    return {{stripe}};
+}
 
-    for (const auto& towel : towels) {
-        if (std::equal(std::begin(towel), std::end(towel),
-                       std::begin(stripe))) {
-            if (isPossible(stripe.substr(towel.size()))) {
-                return true;
+size_t getSize(const std::vector<std::string>& vec) {
+    std::string str;
+    for (const auto& i : vec) {
+        str += i;
+    }
+    return str.size();
+}
+
+uint64_t countPossibleWays(const std::string stripe) {
+    if (stripe.size() == 0) return 0;
+
+    uint64_t ret{};
+
+    auto ways = getWays(stripe);
+    for (const auto& way : ways) {
+        bool success{false};
+        for (const auto& m : way) {
+            for (const auto& towel : towels) {
+                if (towel == m) {
+                    success = true;
+                    break;
+                }
+                //                if (std::equal(std::begin(towel),
+                //                std::end(towel),
+                //                               std::begin(stripe))) {
+                //                    ret +=
+                //                    countPossibleWays(stripe.substr(towel.size()));
+                //                }
             }
         }
+        if (success) ++ret;
     }
+    ret += countPossibleWays(stripe.substr(getSize(ways.at(0))));
 
-    return false;
+    return ret;
 }
 
 uint64_t getResult(const std::string& str) {
@@ -55,9 +118,7 @@ uint64_t getResult(const std::string& str) {
     uint64_t ret{};
 
     for (const auto& stripe : stripes) {
-        if (isPossible(stripe)) {
-            ++ret;
-        }
+        ret += countPossibleWays(stripe);
     }
 
     return ret;
