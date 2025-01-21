04eb1f484544dc78522212965ada02039f9e2e42
Eero Viittala <eero.viittala@outlook.com>
2025-01-21 22:19:17 +0200

    Day21 part2

--- day21/part2/Main.cpp
+++ day21/part2/Main.cpp
@@ -21,13 +21,13 @@ struct Pos {
     }
 };
 
+constexpr uint8_t numberOfDirectionalRobots{25};
+
 Pos pos0;
-Pos pos1;
-Pos pos2;
+Pos posDir[numberOfDirectionalRobots];
 
 const std::vector<std::string> robot0{{"789"}, {"456"}, {"123"}, {"X0A"}};
 const std::vector<std::string> robot1{{"X^A"}, {"<v>"}};
-const std::vector<std::string> robot2{{"X^A"}, {"<v>"}};
 
 Pos move(const Pos pos, const char cmd) {
     switch (cmd) {
@@ -54,17 +54,17 @@ void move_robot0(const char cmd) {
 
 void move_robot1(const char cmd) {
     if (cmd == 'A') {
-        move_robot0(robot1.at(pos1.r)[pos1.c]);
+        move_robot0(robot1.at(posDir[0].r)[posDir[0].c]);
     } else {
-        pos1 = move(pos1, cmd);
+        posDir[0] = move(posDir[0], cmd);
     }
 }
 
 void move_robot2(const char cmd) {
     if (cmd == 'A') {
-        move_robot1(robot2.at(pos2.r)[pos2.c]);
+        move_robot1(robot1.at(posDir[1].r)[posDir[1].c]);
     } else {
-        pos2 = move(pos2, cmd);
+        posDir[1] = move(posDir[1], cmd);
     }
 }
 
@@ -119,41 +119,6 @@ std::vector<std::string> findShortest(const char c,
     return routes;
 }
 
-void addRoute(std::vector<std::string>& vec,
-              const std::vector<std::string>& src) {
-    if (vec.empty()) {
-        if (src.empty()) {
-            vec.push_back("A");
-        } else {
-            for (const auto& s : src) {
-                vec.push_back(s + 'A');
-            }
-        }
-    } else {
-        if (src.empty()) {
-            std::for_each(std::begin(vec), std::end(vec),
-                          [](std::string& str) { str += "A"; });
-
-        } else {
-            const size_t size = vec.size();
-            std::cout << "size: " << src.size() << " vec size: " << vec.size()
-                      << std::endl;
-            for (size_t i{0}; i < (src.size() - 1); ++i) {
-                std::copy(std::begin(vec), std::end(vec),
-                          std::back_inserter(vec));
-            }
-
-            size_t start{};
-            for (const auto& s : src) {
-                std::for_each(std::begin(vec) + start,
-                              std::begin(vec) + start + size,
-                              [&s](std::string& str) { str += s + "A"; });
-                start += size;
-            }
-        }
-    }
-}
-
 void printVec(const std::string str, const std::vector<std::string>& vec) {
     std::cout << str << ": ";
     for (const auto& v : vec) {
@@ -170,90 +135,69 @@ std::string getShortest(const std::vector<std::string>& str) {
     return *it;
 }
 
+std::map<std::pair<std::string, uint64_t>, uint64_t> mem;
+
+uint64_t getShortestDirectionalRoute(const std::string route,
+                                     const uint64_t idx) {
+    if (idx == numberOfDirectionalRobots) {
+        return route.size();
+    }
+
+    if (mem.contains(std::pair(route, idx))) {
+        return mem.at(std::pair(route, idx));
+    }
+
+    uint64_t ret{};
+    for (const auto c : route) {
+        const auto shortests = findShortest(c, robot1, posDir[idx]);
+        std::vector<uint64_t> nextRoutes;
+        for (const auto& shortest : shortests) {
+            nextRoutes.push_back(
+                getShortestDirectionalRoute(shortest, idx + 1));
+        }
+        ret += *std::min_element(std::begin(nextRoutes), std::end(nextRoutes));
+    }
+    mem[std::pair(route, idx)] = ret;
+    return ret;
+}
+
 uint64_t getResult(const std::string& str) {
     auto data = common::parseLines(str);
     // common::printVecStr(data);
-    std::string cmd =
-        "<vA<AA>>^AvAA<^A>A"  // 0
-        "<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A";
 
     pos0.r = 3;
     pos0.c = 2;
-    pos1.r = 0;
-    pos1.c = 2;
-    pos2.r = 0;
-    pos2.c = 2;
 
-    std::vector<std::string> routes;
-    // const std::vector<std::string> codes{
-    //     {"029A"}, {"980A"}, {"179A"}, {"456A"}, {"379A"}};
-    //  const std::string code = "2";
+    for (size_t i{}; i < numberOfDirectionalRobots; ++i) {
+        posDir[i].r = 0;
+        posDir[i].c = 2;
+    }
+
+    std::vector<uint64_t> routes;
     for (const auto& code : data) {
-        std::string route;
+        uint64_t route{};
         for (const char in : code) {
-            std::cout << "Searching: " << in << std::endl;
+            // std::cout << "Searching: " << in << std::endl;
             auto routes0 = findShortest(in, robot0, pos0);
             // printVec("routes0", routes0);
-            std::string shortest;
-            Pos pos1_t(pos1);
+            std::vector<uint64_t> shortest;
             for (const auto& route0 : routes0) {
-                pos1 = pos1_t;
-                std::string temp;
-                for (const char c0 : route0) {
-                    const auto routes1 = findShortest(c0, robot1, pos1);
-                    // printVec("routes1", routes1);
-                    std::vector<std::string> routes3;
-                    Pos pos2_t(pos2);
-                    for (const auto& route1 : routes1) {
-                        pos2 = pos2_t;
-                        std::string routes2_t;
-                        for (const char c1 : route1) {
-                            const auto routes2 = findShortest(c1, robot2, pos2);
-                            // printVec("routes2", routes2);
-                            routes2_t += getShortest(routes2);
-                        }
-                        routes3.push_back(routes2_t);
-                    }
-                    temp += getShortest(routes3);
-                }
-
-                if (shortest.empty() or temp.size() < shortest.size()) {
-                    shortest = temp;
-                }
+                shortest.push_back(getShortestDirectionalRoute(route0, 0));
             }
-            route += shortest;
+            route +=
+                *std::min_element(std::begin(shortest), std::end(shortest));
         }
         routes.push_back(route);
     }
 
     for (size_t i{}; i < routes.size(); ++i) {
-        std::cout << "Code: " << data.at(i) << " Route: " << routes.at(i)
-                  << " size: " << routes.at(i).size() << std::endl;
+        std::cout << "Code: " << data.at(i) << " Route size: " << routes.at(i)
+                  << std::endl;
     }
-
-    for (const auto& route : routes) {
-        pos0.r = 3;
-        pos0.c = 2;
-        pos1.r = 0;
-        pos1.c = 2;
-        pos2.r = 0;
-        pos2.c = 2;
-
-        uint64_t counter{};
-        for (auto c : route) {
-            // std::cout << ++counter << ". cmd: " << c << std::endl;
-            move_robot2(c);
-            // printPos(pos0);
-            // printPos(pos1);
-            // printPos(pos2);
-        }
-    }
-    std::cout << std::endl;
 
     uint64_t res{};
-
     for (size_t i{}; i < routes.size(); ++i) {
-        res += routes.at(i).size() * std::stoi(data.at(i));
+        res += routes.at(i) * std::stoi(data.at(i));
     }
 
     return res;
